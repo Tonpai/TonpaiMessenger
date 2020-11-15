1cb93dd19ec9673b96ae078779b34db730ffd32a
Tanadon Ratsameephen <tonpai@Macintosh.local>
2020-11-15 18:28:25 +0700

    Save checkpoint

--- README.md
+++ README.md
@@ -1,14 +1,22 @@
 # **Tonpai Messenger**
 Tonpai Chat includes server and client for chat application on website. The application based on websocket protocol. Code is written by basic JavaScript. Server runs on NodeJS. And, Client is written by plain Javascipt in HTML file.
 
+## **Latest Update (V 0.01)**
+- Update authentication by combined PassportJS, ExpressJS
+- Merge client.js to ExpressJS
+
 ## **How to run application**
 1. Install NodeJS
 2. Route command line path to project directory
 3. Run `npm start` for start server
-4. Click in `client.html` for run client on browser
+4. Browse `http://localhost:3000/` for join chat page
 
 ## **TODO**
-- Build new client UI
-- Date/Time in bubble chat
+- Login Page
+- Register Page
 - User's authentication
+- Build new client UI
 - Database system
+- Merge Websocket to ExpressJS
+- Date/Time in bubble chat
+- Separate Express's Part and Websocket's Part
--- index.js
+++ index.js
@@ -1,11 +1,76 @@
-const WebSocket = require('ws');
+const express = require('express');
+const passport = require('passport');
+const path = require('path');
 
-// Initialize server.
-const wss = new WebSocket.Server({ port: 8080 });
+// TODO: Authentication
+// - Add passport module to express
+
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+
+passport.serializeUser(function(user, done) {
+    // console.log('serialzeUser');
+    // console.log(`user:`);
+    // console.log(user);
+    done(null, user);
+});
+  
+// passport.deserializeUser(function(user, done) {
+//     console.log(`user: ${user}`);
+//     done(null, user);
+// });
+
+const LocalStrategy = require('passport-local').Strategy;
+
+passport.use(new LocalStrategy(
+    function(username, password, done) {
+        // console.log(`username: ${username}`);
+        // console.log(`password: ${password}`);
+        return done(null, {username: username});
+    }
+));
+
+// GET method route
+app.get('/', function (req, res) {
+    // res.sendFile(path.join(__dirname + '/client.html'));
+    res.render('login');
+});
+
+app.get('/about', function(req, res) {
+    res.render("about");
+})
+
+app.post('/login', passport.authenticate('local'), function(req, res){
+    // console.log(req.user.username)
+    res.redirect('/');
+})
+
+app.get(
+    '/info', 
+    require('connect-ensure-login').ensureLoggedIn(),
+    function(req,res){
+        console.log(req.user);
+        res.send(`user:`);
+    }
+);
+
+app.listen(port, () => {
+    console.log(`Webserver listening at: ${port}`);
+})
+
+const WebSocket = require('ws');
+const wss = new WebSocket.Server({ port: 8080 }); // Websocket Server
 
 // Connection is start
-// ws เป็น connection ระหว่าง server กับ client หนึ่ง ๆ
-// ถ้าต้องการทำ broadcase ต้องใช้ wss (Websocket Server Object) ส่งหา client ทุกคน
+// ws is client
+// If you to broadcast some message to all client, you need to loop over all client.s
 wss.on('connection', function connection(ws) {
     // เมื่อเปิดเซิฟเวอร์ แล้วส่งข้อความหา User นั้น
     ws.send('Connection is start');
